fe927707c6a7508046c9b3cf15d9f868503df19d
Dave Campbell <d.campbell@gns.cri.nz>
2015-06-24 16:17:44 +1200

    add read xml functions

--- get-config.go
+++ get-config.go
@@ -5,25 +5,42 @@ import (
 	"os"
 	"fmt"
 	"bytes"
+	"io/ioutil"
+	"encoding/xml"
 )
+type Mark struct {
+	XMLName xml.Name `xml:"mark"`
+	ID string      `xml:"geodetic-code"`
+	Dome string   `xml:"domes-number"`
+}
+type Site struct {
+	XMLName xml.Name `xml:"site"`
+	Marks []Mark `xml:"mark"`	
+}
 
+ func (m Mark) String() string {
+         return fmt.Sprintf("\t Code : %s - Dome : %s \n", m.ID,  m.Dome)
+ }
 func f(x string) string { 
 	xmlFile, err := os.Open(x)
 	if err != nil {
 		var buffer bytes.Buffer
 		buffer.WriteString("Error opening file:")		
 		buffer.WriteString(x)
-//		y := "Error opening file:"
-//		y += x		
 		y := buffer.String()
 		return y
 	}
 	y := xmlFile.Name()
 	defer xmlFile.Close()	
+	XMLdata, _ := ioutil.ReadAll(xmlFile)
+	var s Site
+	xml.Unmarshal(XMLdata, &s)
+	fmt.Println(s.Marks)
 	return y
 }
 func main() {
 	fmt.Println("Hello World!")
-	z := f("Sites.xml")
+	z := f("Site.xml")
+	
 	fmt.Println(z)
 }
