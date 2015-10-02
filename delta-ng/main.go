fd998ab7659526a725ed4a7bc10a9a683671c1b6
Dave Campbell <d.campbell@gns.cri.nz>
2015-10-02 16:20:09 +1300

    A quick app to create toml site files

---
+++ delta-ng/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"bufio"
+    "os"
+	"strings"
+)
+
+import (
+//        "github.com/zone"
+)
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+func main() {
+
+
+file, err := os.Open("/home/davec/go/src/github.com/go-gps/delta-ng/DELTA_MARK.csv")
+if err != nil {
+    log.Fatal(err)
+}
+defer file.Close()
+
+scanner := bufio.NewScanner(file)
+for scanner.Scan() {
+//    fmt.Println(scanner.Text())
+	result := strings.Split(scanner.Text(), ",")
+//    for i := range result {
+//		fmt.Println(result[i])
+//    }
+		var fn = "/home/davec/go/src/github.com/go-gps/delta-ng/sites/"+strings.ToLower(result[0])+".toml"
+		var f, err = os.Create(fn)
+    	check(err)
+		defer f.Close()
+		w := bufio.NewWriter(f)
+		o := fmt.Sprintf("Code = \"%s\"\nName = \"%s\"\nLatitude = %s\nLongitude =%s\nHeight = %s\nGroundRelationship = %s\nOpened = %s\nClosed = %s\nDOME = \"%s\"\nNetwork = \"%s\"\nMonument = \"%s\"\n",result[0],result[1],result[2],result[3],result[4],result[5],result[6],result[7],result[8],result[9],result[10])
+		_, err = w.WriteString(o)
+		check(err)
+		w.Flush()
+}
+	if err := scanner.Err(); err != nil {
+    	log.Fatal(err)
+	}
+}
